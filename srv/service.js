b7387dbea191c305b3a916b6203651c71e6fbace
Harikrishna Giduthuri <hgiduthuri@abeam.com>
2026-01-19 05:10:21 +0000

    Sync Commit @1/19/2026, 5:10:21 AM

---
+++ srv/service.js
@@ -0,0 +1,13 @@
+const cds = require('@sap/cds');
+
+module.exports = cds.service.impl(async function () {
+  const { A_BusinessPartner } = this.entities;
+  const bupa = await cds.connect.to('BusinessPartnerA2X');
+
+  this.on('READ', A_BusinessPartner, async (req) => {
+    return await bupa.run(req.query);
+  });
+});
+
+
+
